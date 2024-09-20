2e8c51aa5843beaed0f63f3debcd8389665f2c86
wilsone24 <wilsone@uninorte.edu.co>
2024-09-20 11:40:41 -0500

    Delete space

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
