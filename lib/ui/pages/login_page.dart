7d8b930f10465838b745a5a02aab05edfbcc5a1e
wilsone24 <wilsone@uninorte.edu.co>
2024-09-21 02:05:00 -0500

    Refactor main.dart and create LoginPage
    - Refactor main.dart to update the app title and theme
    - Create LoginPage widget for user login functionality

---
+++ lib/ui/pages/login_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const buttonColor = Color(0xFF74B7BB);
+    return Column(
+      children: [
+        const SizedBox(
+          height: 300,
+        ),
+        const Text('Enter your name',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.w500)),
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromARGB(255, 242, 241, 241),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+              suffixIcon: const Icon(Icons.login, color: buttonColor),
+              hintStyle: TextStyle(color: Colors.grey[400]),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  borderSide:  const BorderSide(color: Colors.red)),
+              labelText: 'Input Name',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsetsDirectional.symmetric(horizontal: 110),
+          child: SizedBox(
+            width: double.infinity,
+            child: FilledButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(buttonColor)),
+                child: const Padding(
+                  padding: EdgeInsetsDirectional.symmetric(vertical: 8),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                )),
+          ),
+        ),
+      ],
+    );
+  }
+}
