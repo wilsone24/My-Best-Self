1e4a32c9e1e0a363081eee86a6402c4f78c17e11
wilsone24 <wilsone@uninorte.edu.co>
2024-09-23 13:06:44 -0500

    Refactor profile_page.dart: Adjust container height and add box shadow

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -36,16 +36,9 @@ class ProfilePage extends StatelessWidget {
                   height: 80,
                   width: 220,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.1),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3) // changes
-                            )
-                      ]),
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
+                  ),
                   child: Padding(
                     padding:
                         const EdgeInsetsDirectional.only(start: 25, end: 20),
