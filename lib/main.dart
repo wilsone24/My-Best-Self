abdf96dd8d499620abf98a9d04f98aa0aeb6f043
Lofo <fmarinl@uninorte.edu.co>
2024-09-23 10:02:12 -0500

    Refactor main.dart: Replace LoginPage with ProfilePage and add HomePage Refactor profile_page.dart: Add Footer widget Add CustomBottomNavBar widget to navbar.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_best_self/ui/navbar/navbar.dart';
 import 'package:my_best_self/ui/pages/login_page.dart';
-import 'package:my_best_self/ui/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +12,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'GetX Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF74B7BB)),
-          useMaterial3: true,
-        ),
-        home: const Scaffold(
-          backgroundColor: Color(0xFFF9F9F9),
-          body: ProfilePage(),
-        ));
+      title: 'GetX Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF74B7BB)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF9F9F9),
+      body: Center(
+        child: Text('Content for index $_selectedIndex'),
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
   }
 }
--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_best_self/ui/navbar/navbar.dart';
 import 'package:my_best_self/ui/utils/colors.dart';
 import 'package:my_best_self/ui/widgets/custom_button.dart';
 
@@ -11,19 +12,22 @@ class ProfilePage extends StatelessWidget {
       children: [
         SafeArea(
           child: Container(
-              width: double.infinity,
-              height: 260,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40)),
+            width: double.infinity,
+            height: 260,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
               ),
-              child: const Center(
-                  child: Text(
+            ),
+            child: const Center(
+              child: Text(
                 "Hi Wilson!",
                 style: TextStyle(fontSize: 45, fontWeight: FontWeight.w600),
-              ))),
+              ),
+            ),
+          ),
         ),
         Padding(
           padding: const EdgeInsetsDirectional.only(
@@ -32,41 +36,49 @@ class ProfilePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                  height: 80,
-                  width: 220,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: Colors.white,
+                height: 80,
+                width: 220,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 25, end: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "21/09/2024",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                          Text(
+                            "Total points",
+                            style: TextStyle(
+                                color: Colors.grey[600], fontSize: 17),
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        "100",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: primarycolor),
+                      )
+                    ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.only(start: 25,end: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "21/09/2024",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 17),
-                            ),
-                            Text("Total points",
-                                style: TextStyle(
-                                    color: Colors.grey[600], fontSize: 17)),
-                          ],
-                        ),
-                        const Text("100", style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20, color: primarycolor
-                        ),)
-                      ],
-                    ),
-                  )),
+                ),
+              ),
               Image.asset("assets/profile.png", height: 148, width: 148),
             ],
           ),
         ),
         const SafeArea(child: CustomButton(text: "Logout")),
+        Footer(), // Aquí se agrega el Footer
       ],
     );
   }
